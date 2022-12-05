e075713177bcc7ade3c51376df1208b00e45ca9e
yurii.hliuza@gmail.com <yurii.hliuza@gmail.com>
2022-12-05 09:56:28 +0200

    Lab 3 complete

---
+++ Lab3/Program.cs
@@ -0,0 +1,111 @@
+﻿using Eto.Forms;
+using LiveChartsCore.Defaults;
+using System.Collections.ObjectModel;
+
+namespace Lab3
+{
+    class Program
+    {
+        public static List<(double Sum, double Time)> Data { get; set; }
+        public static (double CenterWeightX, double CenterWeightY) CenterWeight { get; set; }
+
+        [STAThread]
+        public static void Main()
+        {
+            Console.WriteLine("Enter name of file: ");
+            string inputFilename = Console.ReadLine();
+            if (File.Exists(inputFilename))
+            {
+                Data = DataFromFile(inputFilename);
+
+                CenterWeight = CalculateCenterWeight(Data);
+                Console.WriteLine($"Center of weight = ({CenterWeight.CenterWeightX}; {CenterWeight.CenterWeightY})");
+
+                Console.WriteLine($"Covariation = {Cov(Data, CenterWeight)}");
+
+                Console.WriteLine(WriteEquelRegression(Data, CenterWeight));
+
+                Console.WriteLine($"Corelation coeficient = {Corelation(Data, CenterWeight)}");
+
+                Application application = new();
+                application.Run(new ScatterDiagramWindow(Data));
+            }
+            else
+                Console.WriteLine("Wrong filename");
+        }
+
+        public static List<(double Sum, double Time)> DataFromFile(string fileName)
+        {
+            List<(double Sum, double Time)> data = new List<(double Sum, double Time)>();
+
+            using (StreamReader reader = new StreamReader(fileName))
+            {
+                string? line;
+                int.TryParse(reader.ReadLine(), out int quantity);
+                while ((line = reader.ReadLine()) is not null)
+                {
+                    string[] columns = line.Split('\t');
+
+                    if (double.TryParse(columns[0], out double sum))
+                    {
+                        data.Add((sum, double.Parse(columns[1])));
+                    }
+                }
+            }
+
+            return data;
+        }
+        
+        public static (double CenterWeightX, double CenterWeightY) CalculateCenterWeight(List<(double Sum, double Time)> data)
+        {
+            return (CenterWeightX: Avarage(data.Select(i => i.Time)), CenterWeightY: Avarage(data.Select(i => i.Sum)));
+        }
+        public static double Avarage(IEnumerable<double> values) => values.Sum() / values.Count();
+        
+        public static double Cov(List<(double Sum, double Time)> data, (double CenterWeightX, double CenterWeightY) centerWeight)
+        {
+            double res = 0;
+            foreach (var item in data)
+            {
+                res += item.Sum * item.Time;
+            }
+            res /= data.Count;
+            res -= centerWeight.CenterWeightX * centerWeight.CenterWeightY;
+
+            return res;
+        }
+
+        #region RegressionLine
+        public static string WriteEquelRegression(List<(double Sum, double Time)> data, (double CenterWeightX, double CenterWeightY) centerWeight)
+        {
+            double varX = Var(data, centerWeight);
+
+            double m = Cov(data, centerWeight) / varX;
+
+            return $"y = {m}x - {m * centerWeight.CenterWeightX + centerWeight.CenterWeightY}";
+        }
+
+        public static double Var(List<(double Sum, double Time)> data, (double CenterWeightX, double CenterWeightY) centerWeight)
+        {
+            double res = data.Sum(i => Math.Pow(i.Time, 2)) / data.Count();
+            res -= Math.Pow(centerWeight.CenterWeightX, 2);
+
+            return res;
+        }
+        #endregion
+
+        public static double Corelation(List<(double Sum, double Time)> data, (double CenterWeightX, double CenterWeightY) centerWeight)
+        {
+            double numerator = 0, sigmXExpr = 0, sigmYExpr = 0;
+
+            foreach (var item in data)
+            {
+                numerator += (item.Time - centerWeight.CenterWeightX) * (item.Sum - centerWeight.CenterWeightY);
+                sigmXExpr += Math.Pow(item.Time - centerWeight.CenterWeightX, 2);
+                sigmYExpr += Math.Pow(item.Sum - centerWeight.CenterWeightY, 2);
+            }
+
+            return numerator / Math.Sqrt(sigmXExpr * sigmYExpr);
+        }
+    }
+}
---
+++ Lab3/ScatterDiagramWindow.cs
@@ -0,0 +1,95 @@
+﻿using Eto.Drawing;
+using Eto.Forms;
+using LiveChartsCore;
+using LiveChartsCore.Defaults;
+using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore.SkiaSharpView.Eto;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    internal class ScatterDiagramWindow : Form
+    {
+        public ScatterDiagramWindow(List<(double Sum, double Time)> data)
+        {
+            Title = "Lab3";
+            MinimumSize = new Size(200, 100);
+            Size = MinimumSize * 4;
+            Padding = 10;
+
+            //this.Content = new BoxChartPanel(max, q3, q1, min);
+            this.Content = new ScatterPanel(data);
+        }
+    }
+
+
+    //public partial class ScatterDiagram
+    //{
+    //    public ISeries[] Series { get; set; } =
+    //    {
+    //        new ScatterSeries<ObservablePoint>
+    //        {
+    //            Values = new ObservableCollection<ObservablePoint>
+    //            {
+    //                new(2.2, 5.4),
+    //                new(4.5, 2.5),
+    //                new(4.2, 7.4),
+    //                new(6.4, 9.9),
+    //                new(4.2, 9.2),
+    //                new(5.8, 3.5),
+    //                new(7.3, 5.8),
+    //                new(8.9, 3.9),
+    //                new(6.1, 4.6),
+    //                new(9.4, 7.7),
+    //                new(8.4, 8.5),
+    //                new(3.6, 9.6),
+    //                new(4.4, 6.3),
+    //                new(5.8, 4.8),
+    //                new(6.9, 3.4),
+    //                new(7.6, 1.8),
+    //                new(8.3, 8.3),
+    //                new(9.9, 5.2),
+    //                new(8.1, 4.7),
+    //                new(7.4, 3.9),
+    //                new(6.8, 2.3),
+    //                new(5.3, 7.1),
+    //            }
+    //        }
+    //    };
+    //}
+
+    public class ScatterPanel : Panel
+    {
+        private readonly CartesianChart cartesianChart;
+
+        public ScatterPanel(List<(double Sum, double Time)> data)
+        {
+
+            ObservableCollection<ObservablePoint> points = new();
+            foreach (var item in data)
+            {
+                points.Add(new(item.Time, item.Sum));
+            }
+
+            ISeries[] scatterSeries =
+            {
+                new ScatterSeries<ObservablePoint>
+                {
+                    Values = points
+                }
+            };
+
+            cartesianChart = new CartesianChart
+            {
+                Series = scatterSeries
+            };
+
+            Content = cartesianChart;
+        }
+    }
+}
