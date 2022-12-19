ed3222ec75079a90205f9dd6c8cdba46acfe3cbc
yurii.hliuza@gmail.com <yurii.hliuza@gmail.com>
2022-12-20 01:30:27 +0200

    Added Lab4, Lab5

---
+++ Lab4/Program.cs
@@ -0,0 +1,231 @@
+﻿using System.Numerics;
+
+namespace Lab4;
+
+class Program
+{
+    public static void Main()
+    {
+        Task1();
+        Task2();
+        Task3();
+        Task4();
+        Task5();
+        Task6();
+        Task7();
+        Task8();
+        Task9();
+        Task10();
+    }
+
+    #region Calculation
+    public static int C(int m, int n)
+    {
+        BigInteger num = Factiorial(n) / (Factiorial(m) * Factiorial(n - m));
+        return (int)num;
+    }
+
+    public static BigInteger Factiorial(int a)
+    {
+        BigInteger num = 1;
+        for (int i = 2; i <= a; i++)
+        {
+            num *= i;
+        }
+        return num;
+    }
+
+    public static double BernoulliTrial(int m, int n, double p)
+    {
+        return C(m, n) * Math.Pow(p, m) * Math.Pow(ReverseP(p), n - m);
+    }
+
+    public static double ReverseP(double p)
+    {
+        return 1 - p;
+    }
+
+    public static double BayesTheorem(List<(double p1, double p2)> pAs, int sIndex)
+    {
+        return (pAs[sIndex].p1 * pAs[sIndex].p2) / TotalProbability(pAs);
+    }
+
+    public static double TotalProbability(List<(double p1, double p2)> p)
+    {
+        double totalProbability = 0.0;
+        foreach (var item in p)
+        {
+            totalProbability += item.p1 * item.p2;
+        }
+        return totalProbability;
+    }
+    #endregion
+
+    #region Tasks
+    public static void Task1()
+    {
+        Console.WriteLine("Task1");
+
+        int blackShoes = 40, brownShoes = 26, redShoes = 22, blueShoes = 12;
+        double totalShoes = blackShoes + brownShoes + redShoes + blueShoes;
+        double P = (redShoes + blueShoes) / totalShoes;
+        Console.WriteLine($"P = {P}");
+
+        Console.WriteLine(new String('-', 100));
+        Console.WriteLine("");
+    }
+    public static void Task2()
+    {
+        Console.WriteLine("Task2");
+
+        int employees = 10, consultants = 8;
+        int selectedEmployees = 2;
+        int notConsultants = employees - consultants;
+        double revP = 1.0;
+        for (int i = 0; i < selectedEmployees; i++)
+        {
+            revP *= notConsultants / (double)employees;
+            notConsultants--;
+            employees--;
+        }
+        double P = 1 - revP;
+        Console.WriteLine($"P = {P}");
+
+        Console.WriteLine(new String('-', 100));
+        Console.WriteLine("");
+    }
+    public static void Task3()
+    {
+        Console.WriteLine("Task3");
+
+        int managers = 10, reletives = 2;
+        int selectedManagers = 3;
+        int notReletives = managers - reletives;
+        double revP = 1.0;
+        for (int i = 0; i < selectedManagers; i++)
+        {
+            revP *= notReletives / (double)managers;
+            notReletives--;
+            managers--;
+        }
+        double P = 1 - revP;
+        Console.WriteLine($"P = {P}");
+
+        Console.WriteLine(new String('-', 100));
+        Console.WriteLine("");
+    }
+    public static void Task4()
+    {
+        Console.WriteLine("Task4");
+
+        decimal p1 = 0.15m, p2 = 0.25m, p3 = 0.2m, p4 = 0.1m, p5;
+        p5 = 1 - (p1 + p2 + p3 + p4);
+        Console.WriteLine($"p5 = {p5}");
+
+        Console.WriteLine(new String('-', 100));
+        Console.WriteLine("");
+    }
+
+    public static void Task5()
+    {
+        Console.WriteLine("Task5");
+
+        int railways = 120, trains = 80, selectedTrains = 2;
+        double P = C(selectedTrains, trains) / (double)C(selectedTrains, railways);
+        Console.WriteLine($"P = {P}");
+
+        Console.WriteLine(new String('-', 100));
+        Console.WriteLine("");
+    }
+
+    public static void Task6()
+    {
+        Console.WriteLine("Task6");
+
+        List<(double probabilityToProduce, double probabilityToproduceFirstGrade)> producing = new()
+        {
+            (0.9, 0.8),
+        };
+        Console.WriteLine($"Probability of producing first grade product by this machine: {BayesTheorem(producing, 0)}");
+        Console.WriteLine($"Probability of producing first grade product: {TotalProbability(producing)}");
+
+        Console.WriteLine(new String('-', 100));
+        Console.WriteLine("");
+    }
+
+    public static void Task7()
+    {
+        Console.WriteLine("Task7");
+
+        int students = 10, tests = 20;
+        List<(int sudents, int knowTests)> studentsTypes = new()
+        {
+            (3, 20),
+            (4, 16),
+            (2, 10),
+            (1, 5)
+        };
+
+        List<(double pa, double pba)> pAs = new List<(double pa, double pba)>();
+        foreach (var item in studentsTypes)
+        {
+            pAs.Add((item.sudents / (double)students, (item.knowTests / (double)tests) *
+                                                      ((item.knowTests - 1.0) / ((double)tests - 1.0)) *
+                                                      ((item.knowTests - 2.0) / ((double)tests - 2.0))));
+        }
+        double P1 = BayesTheorem(pAs, 0);
+        double P2 = BayesTheorem(pAs, 3);
+        Console.WriteLine($"Probability that student prepared perfectly: {P1}");
+        Console.WriteLine($"Probability that student prepared bad: {P2}");
+
+        Console.WriteLine(new String('-', 100));
+        Console.WriteLine("");
+    }
+
+    public static void Task8()
+    {
+        Console.WriteLine("Task8");
+
+        List<(double detailProbability, double standartDetailProbability)> details = new()
+        {
+            (0.4, 0.9),
+            (0.3, 0.95),
+            (0.3, 0.95),
+        };
+        Console.WriteLine($"P = {TotalProbability(details)}");
+
+        Console.WriteLine(new String('-', 100));
+        Console.WriteLine("");
+    }
+
+    public static void Task9()
+    {
+        Console.WriteLine("Task9");
+
+        List<(double symptomProbability, double healProbability)> patients = new()
+        {
+            (0.4, 0.8),
+            (0.3, 0.7),
+            (0.3, 0.85),
+        };
+        Console.WriteLine($"P = {BayesTheorem(patients, 1)}");
+
+        Console.WriteLine(new String('-', 100));
+        Console.WriteLine("");
+    }
+    public static void Task10()
+    {
+        Console.WriteLine("Task10");
+
+        List<(double symptomProbability, double healProbability)> specialists = new()
+        {
+            (0.3, 0.9),
+            (0.7, 0.8),
+        };
+        Console.WriteLine($"P = {BayesTheorem(specialists, 0)}");
+
+        Console.WriteLine(new String('-', 100));
+        Console.WriteLine("");
+    }
+    #endregion
+}
---
+++ Lab5/Program.cs
@@ -0,0 +1,221 @@
+﻿using System.Numerics;
+
+namespace Lab5;
+
+class Program
+{
+    public static void Main()
+    {
+        Task1(3, 5, 0.2);
+        Task2(4, 5, 0.8);
+        Task3(80, 400, 0.2);
+        Task4(5, 100000, 0.0001);
+        Task5(228, 252, 600, 0.4);
+        Task6(100, 0.4);
+        Task7(0, 170, 4000, 0.04);
+        Task8(5000, 10000);
+        Task9(5, 1000, 0.002);
+        Task10(150, 0.03m);
+    }
+
+
+    #region Calculation
+    public static int C(int m, int n)
+    {
+        BigInteger num = Factiorial(n) / (Factiorial(m) * Factiorial(n - m));
+        return (int)num;
+    }
+
+    public static BigInteger Factiorial(int a)
+    {
+        BigInteger num = 1;
+        for (int i = 2; i <= a; i++)
+        {
+            num *= i;
+        }
+        return num;
+    }
+
+    public static double BernoulliTrial(int m, int n, double p)
+    {
+        return C(m, n) * Math.Pow(p, m) * Math.Pow(ReverseP(p), n - m);
+    }
+
+    public static double ReverseP(double p)
+    {
+        return 1 - p;
+    }
+
+    public static double LocalDeMoivreLaplaceTheorem(int m, int n, double p)
+    {
+        var x = (m - n * p) / (Math.Sqrt(n * p * ReverseP(p)));
+        var a = 1 / Math.Sqrt(n * p * ReverseP(p));
+        var b = (1 / Math.Sqrt(2 * Math.PI));
+        var c = Math.Pow(Math.E, -(Math.Pow(x, 2) / 2));
+
+        return a * b * c;
+    }
+    public static double IntegralDeMoivreLaplaceTheorem(int m1, int m2, int n, double p)
+    {
+        var x1 = (m1 - n * p) / Math.Sqrt(n * p * ReverseP(p));
+        var x2 = (m2 - n * p) / Math.Sqrt(n * p * ReverseP(p));
+
+        var P = Phi(x2) - Phi(x1);
+        return P;
+    }
+    public static double Phi(double x)
+    {
+        return (1 / Math.Sqrt(2 * Math.PI) * Math.Sqrt(Math.PI / 2) * Erf(x / Math.Sqrt(2)));
+    }
+    public static double Erf(double x)
+    {
+        const double a1 = 0.254829592;
+        const double a2 = -0.284496736;
+        const double a3 = 1.421413741;
+        const double a4 = -1.453152027;
+        const double a5 = 1.061405429;
+        const double p = 0.3275911;
+
+        int sign = 1;
+        if (x < 0)
+            sign = -1;
+        x = Math.Abs(x);
+
+        // A&S formula 7.1.26
+        double t = 1.0 / (1.0 + p * x);
+        double y = 1.0 - (((((a5 * t + a4) * t) + a3) * t + a2) * t + a1) * t * Math.Exp(-x * x);
+
+        return sign * y;
+    }
+
+    public static double PoissonsEquation(int m, int n, double p)
+    {
+        var l = n * p;
+        return (Math.Pow(l, m) / (double)Factiorial(m)) * Math.Pow(Math.E, -l);
+    }
+
+    public static void MostLikelyNumber(int n, decimal p, out decimal m1, out decimal m2)
+    {
+        decimal q = (decimal)ReverseP((double)p);
+        m1 = n * p - q;
+        m2 = n * p + p;
+    }
+
+    #endregion
+
+    #region Tasks
+    public static void Task1(int m, int n, double p)
+    {
+        Console.WriteLine("Task 1");
+        Console.WriteLine($"P = {BernoulliTrial(m, n, p)}");
+        Console.WriteLine(new String('-', 100));
+        Console.WriteLine();
+    }
+
+    public static void Task2(int m, int n, double p)
+    {
+        Console.WriteLine("Task 2");
+        Console.WriteLine($"Event A will be executed exactly {m} times: {BernoulliTrial(m, n, p)}");
+
+        double pNotLessThenM = 0.0;
+        for (int i = n; i >= m; i--)
+        {
+            pNotLessThenM += BernoulliTrial(i, n, p);
+        }
+
+        Console.WriteLine($"Evant A will be executed not less than {m} times: {pNotLessThenM}");
+        Console.WriteLine(new String('-', 100));
+        Console.WriteLine();
+    }
+
+    public static void Task3(int m, int n, double p)
+    {
+        Console.WriteLine("Task 3");
+        Console.WriteLine($"Probability that among {n} candle will be exactly {m} lollipops: {LocalDeMoivreLaplaceTheorem(m, n, p)}");
+        Console.WriteLine(new String('-', 100));
+        Console.WriteLine();
+    }
+
+    public static void Task4(int m, int n, double p)
+    {
+        Console.WriteLine("Task 4");
+        Console.WriteLine($"P = {PoissonsEquation(m, n, p)}");
+        //Console.WriteLine($"Poisson's version: {PoissonsEquation(m, n, p)}");
+        //Console.WriteLine(new String('.', 50));
+        //Console.WriteLine($"Laplace's version: {LocalDeMoivreLaplaceTheorem(m, n, p)}");
+        Console.WriteLine(new String('-', 100));
+        Console.WriteLine();
+    }
+
+    public static void Task5(int m1, int m2, int n, double p)
+    {
+        Console.WriteLine("Task 5");
+        var P = IntegralDeMoivreLaplaceTheorem(m1, m2, n, p);
+        Console.WriteLine($"P = {P}");
+        Console.WriteLine(new String('-', 100));
+        Console.WriteLine();
+    }
+
+    public static void Task6(int n, double p)
+    {
+        Console.WriteLine("Task 6");
+        MostLikelyNumber(n, (decimal)p, out decimal m1, out decimal m2);
+        if (m1 == Math.Ceiling(m1) && m2 == Math.Ceiling(m2))
+        {
+            double P = LocalDeMoivreLaplaceTheorem((int)Math.Ceiling(m1), n, p);
+            Console.WriteLine($"Probability of most likely number of clints every day ({m1}, {m2}) is: {P}");
+        }
+        else
+        {
+            double P = LocalDeMoivreLaplaceTheorem((int)Math.Ceiling(m1), n, p); ;
+            Console.WriteLine($"Probability of most likely number of clints every day ({Math.Ceiling(m1)}) is: {P}");
+        }
+
+        Console.WriteLine(new String('-', 100));
+        Console.WriteLine();
+    }
+
+    public static void Task7(int m1, int m2, int n, double p)
+    {
+        Console.WriteLine("Task 7");
+        var P = IntegralDeMoivreLaplaceTheorem(m1, m2, n, p);
+        Console.WriteLine($"P = {P}");
+        Console.WriteLine(new String('-', 100));
+        Console.WriteLine();
+    }
+
+    public static void Task8(int m, int n)
+    {
+        Console.WriteLine("Task 8");
+        double p = 0.5;
+        Console.WriteLine($"P = {LocalDeMoivreLaplaceTheorem(m, n, p)}");
+        Console.WriteLine(new String('-', 100));
+        Console.WriteLine();
+    }
+
+    public static void Task9(int m, int n, double p)
+    {
+        Console.WriteLine("Task 9");
+        Console.WriteLine($"P = {PoissonsEquation(m, n, p)}");
+        Console.WriteLine(new String('-', 100));
+        Console.WriteLine();
+    }
+
+    public static void Task10(int n, decimal q)
+    {
+        Console.WriteLine("Task 10");
+        decimal p = (decimal)ReverseP((double)q);
+        MostLikelyNumber(n, p, out decimal m1, out decimal m2);
+        if (m1 == Math.Ceiling(m1) && m2 == Math.Ceiling(m2))
+        {
+            Console.WriteLine($"m1 = {m1}");
+            Console.WriteLine($"m2 = {m2}");
+        }
+        else
+            Console.WriteLine($"m = {Math.Ceiling(m1)}");
+
+        Console.WriteLine(new String('-', 100));
+    }
+
+    #endregion
+}
